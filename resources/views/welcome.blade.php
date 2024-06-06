e62dc94f4be96646734a6800e2031d81804b22a7
Betho <hgomescostajr@gmail.com>
2024-06-06 19:28:03 -0300

    feat: add logo in header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 		<title>My Vape Store</title>
 		<meta charset="utf-8" />
 		<meta name="theme-color" content="#EA60D6">
-		<link rel="icon" href="images/icon.png">
+		<link rel="icon" href="images/logo.jpeg">
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
