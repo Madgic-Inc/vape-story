5bc5b10e584764151ff0bead4076166ea7250fe7
Betho <hgomescostajr@gmail.com>
2024-06-07 00:25:47 -0300

    teste

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,22 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Crie usuários aqui com o método DB::table('users')->insert()
+        DB::table('users')->insert([
+            [
+                'name' => 'Taynara',
+                'email' => 'taynara@gmail.com',
+                'password' => bcrypt('taynara2024'),
+            ],
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('show-products',  [App\Http\Controllers\HomeController::class, 'showProducts'])->name('showProducts');
+
+Route::get('create-product',  [App\Http\Controllers\HomeController::class, 'createProduct'])->name('createProduct');
+Route::post('create-product',  [App\Http\Controllers\HomeController::class, 'storeProduct'])->name('storeProduct');
